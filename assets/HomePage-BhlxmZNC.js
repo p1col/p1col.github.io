3634686749d9e1fc0294ebca7613d1e294c489bd
p1col <162457850+p1col@users.noreply.github.com>
2024-05-14 03:44:49 +0000

    Deploying to gh-pages from @ p1col/p1col.github.io@6d2b3de72e38967158771c8baffc49117a75fa38 🚀

--- assets/HomePage-BhlxmZNC.js
+++
@@ -1,1 +0,0 @@
-import{d as u,c as a,a as n,u as c,o as d}from"./index-BkS4C35t.js";const _={class:"entry-group"},i=u({__name:"HomePage",setup(l){const s=c();function e(o){s.push(o)}return(o,t)=>(d(),a("div",_,[n("button",{class:"entry__btn",onClick:t[0]||(t[0]=r=>e("/sudoku"))},"sudoku"),n("button",{class:"entry__btn",onClick:t[1]||(t[1]=r=>e("/card"))},"card")]))}});export{i as default};
